ccac7a0804240835f106f58d7ea22fa9d023ef9c
Justin.dev <justinbina515@gmail.com>
2026-07-16 18:01:12 +0000

    revert: remove filter providers added during audit

--- e_commerce/lib/domain/wiewmodel/provider/filter_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-// Gère le texte de recherche
-final searchQueryProvider = StateProvider<String>((ref) => "");
-
-// Gère la catégorie sélectionnée (null = toutes)
-final selectedCategoryProvider = StateProvider<String?>((ref) => null);
