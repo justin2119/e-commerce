dc0c75a394a4ea00c957cf55b610f8b9850b14e2
Justin.dev <justinbina515@gmail.com>
2026-07-16 18:02:19 +0000

    revert: restore original main.dart state

--- e_commerce/lib/main.dart
+++ e_commerce/lib/main.dart
@@ -1,37 +1,21 @@
 import 'package:e_commerce/ui/home.dart';
-import 'package:e_commerce/utils/routes.dart';
-import 'package:e_commerce/domain/wiewmodel/provider/router_provider.dart';
-import 'package:e_commerce/domain/wiewmodel/provider/theme_provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main(){
   runApp(
-    const ProviderScope(child: MyApp()),
+    ProviderScope(child: MyApp())
   );
 }
-
-class MyApp extends ConsumerWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final router = ref.watch(routerProvider);
-    final themeMode = ref.watch(themeProvider);
-    return MaterialApp.router(
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      themeMode: themeMode,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        useMaterial3: true,
-      ),
-      darkTheme: ThemeData.dark(useMaterial3: true).copyWith(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepOrange,
-          brightness: Brightness.dark,
-        ),
-      ),
-      routerConfig: router,
+      home: const ProductScreen(),
     );
   }
 }
