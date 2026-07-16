e88d20de79c5aca12c12cb65f89d70404a53fa72
Justin.dev <justinbina515@gmail.com>
2026-07-16 17:48:01 +0000

    feat: support light/dark theme in main.dart

--- e_commerce/lib/main.dart
+++ e_commerce/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:e_commerce/domain/wiewmodel/provider/router_provider.dart';
+import 'package:e_commerce/domain/wiewmodel/provider/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main(){
+void main() {
   runApp(
-    ProviderScope(child: MyApp())
+    const ProviderScope(child: MyApp()),
   );
 }
 
@@ -14,12 +15,20 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(routerProvider);
+    final themeMode = ref.watch(themeProvider);
     
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      themeMode: themeMode,
       theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         useMaterial3: true,
+      ),
+      darkTheme: ThemeData.dark(useMaterial3: true).copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepOrange,
+          brightness: Brightness.dark,
+        ),
       ),
       routerConfig: router,
     );
