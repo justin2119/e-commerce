863255e300e29ecb460667600c504e0e180d1855
Justin.dev <justinbina515@gmail.com>
2026-07-16 13:21:23 +0000

    refactor(main): integrate GoRouter with MaterialApp.router

--- e_commerce/lib/main.dart
+++ e_commerce/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:e_commerce/ui/main_view.dart';
+import 'package:e_commerce/domain/wiewmodel/provider/router_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -8,18 +8,20 @@ void main(){
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+    
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
         useMaterial3: true,
       ),
-      home: const MainView(),
+      routerConfig: router,
     );
   }
 }
