4eea0cb7ec7ae518938721968c25199eeb14212e
Justin.dev <justinbina515@gmail.com>
2026-07-16 21:28:42 +0000

    refactor: update RecommendeWidget to display real product data

--- e_commerce/lib/ui/widgets/recommende_widget.dart
+++ e_commerce/lib/ui/widgets/recommende_widget.dart
@@ -1,41 +1,94 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import '../../domain/models/product.dart';
 
 class RecommendeWidget extends StatelessWidget {
-  const RecommendeWidget({Key? key}) : super(key: key);
+  final List<Product> products;
+  const RecommendeWidget({Key? key, required this.products}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: GridView(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
               crossAxisSpacing: 10,
-              mainAxisSpacing: 10
+              mainAxisSpacing: 10,
+              childAspectRatio: 0.75,
           ),
-        children: [
-          Stack(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          final product = products[index];
+          return Stack(
             children: [
               Container(
-                width: MediaQuery.of(context).size.width*0.5,
-                height: MediaQuery.of(context).size.height,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
-                  color: Colors.deepOrange
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey.shade200),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade100,
+                      blurRadius: 5,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: ClipRRect(
+                        borderRadius: const BorderRadius.vertical(top: Radius.circular(10)),
+                        child: Image.network(
+                          product.imageUrl,
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                          errorBuilder: (context, error, stackTrace) => 
+                            Container(color: Colors.grey.shade200, child: const Icon(Icons.error)),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            product.name,
+                            style: GoogleFonts.abel(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Text(
+                            "${product.price} €",
+                            style: GoogleFonts.abel(
+                              fontSize: 16,
+                              color: Colors.deepOrange,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
               Positioned(
                   top: 10,
                   left: 10,
                   child: Container(
-                    padding: EdgeInsets.all(5),
+                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
                       color: Colors.black
                     ),
                     child: Text("New",
                       style: GoogleFonts.abel(
-                        fontSize: 20,
+                        fontSize: 14,
                         color: Colors.white,
                         fontWeight: FontWeight.bold
                       ),
@@ -43,118 +96,16 @@ class RecommendeWidget extends StatelessWidget {
                   )
               ),
               Positioned(
-                  bottom: 10,
-                  right: 10,
-                  child: Icon(Icons.favorite,color: Colors.white,size: 30,)
-              ),
-            ]
-          ),
-          Stack(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width*0.5,
-                height: MediaQuery.of(context).size.height,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.deepOrange
-                ),
-              ),
-              Positioned(
-                  top: 10,
-                  left: 10,
-                  child: Container(
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.black
-                    ),
-                    child: Text("New",
-                      style: GoogleFonts.abel(
-                        fontSize: 20,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
+                  top: 5,
+                  right: 5,
+                  child: IconButton(
+                    icon: const Icon(Icons.favorite_border, color: Colors.deepOrange),
+                    onPressed: () {},
                   )
               ),
-              Positioned(
-                  bottom: 10,
-                  right: 10,
-                  child: Icon(Icons.favorite,color: Colors.white,size: 30,)
-              ),
             ]
-          ),
-          Stack(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width*0.5,
-                height: MediaQuery.of(context).size.height,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.deepOrange
-                ),
-              ),
-              Positioned(
-                  top: 10,
-                  left: 10,
-                  child: Container(
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.black
-                    ),
-                    child: Text("New",
-                      style: GoogleFonts.abel(
-                        fontSize: 20,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                  )
-              ),
-              Positioned(
-                  bottom: 10,
-                  right: 10,
-                  child: Icon(Icons.favorite,color: Colors.white,size: 30,)
-              ),
-            ]
-          ),
-          Stack(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width*0.5,
-                height: MediaQuery.of(context).size.height,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.deepOrange
-                ),
-              ),
-              Positioned(
-                  top: 10,
-                  left: 10,
-                  child: Container(
-                    padding: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.black
-                    ),
-                    child: Text("New",
-                      style: GoogleFonts.abel(
-                        fontSize: 20,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                  )
-              ),
-              Positioned(
-                  bottom: 10,
-                  right: 10,
-                  child: Icon(Icons.favorite,color:Colors.white,size: 30,)
-              ),
-            ]
-          ),
-        ],
+          );
+        },
       ),
     );
   }
